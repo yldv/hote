066f24cb91a1362397c7128b8c7d97e34ac7e427
yldv <abdulazizy5955@icloud.com>
2025-06-11 10:58:25 +0500

    ++

--- config/settings.py
+++ config/settings.py
@@ -9,7 +9,9 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
+from dotenv import load_dotenv
 import os
+load_dotenv()
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
